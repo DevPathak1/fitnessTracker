7b16f43b26fad10874cd04eff5ca816ed1d03155
Jack He <jackhe04@gmail.com>
2024-11-07 09:22:19 -0800

    change exercise entity

--- lib/entity/exercise.dart
+++ lib/entity/exercise.dart
@@ -1,9 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Exercise {
   final String name;
   final String? type;
-  final int sets;
+  final List<ExerciseSet> sets;
+
+  Exercise(this.name, this.type, this.sets);
+  Exercise.init(this.name, this.type, int nSets, int reps, double weight)
+      : sets = List.generate(nSets, (_) => ExerciseSet(reps, weight));
+
+  factory Exercise.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Exercise(
+        data?['name'],
+        data?['type'],
+        List.generate(data?['sets'].length ?? 0,
+            (i) => ExerciseSet(data!['sets'][i].reps, data['sets'][i].weight)));
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      if (type != null) 'type': type,
+      'sets': sets,
+    };
+  }
+}
+
+class ExerciseSet {
   final int reps;
   final double weight;
 
-  Exercise(this.name, this.type, this.sets, this.reps, this.weight);
+  ExerciseSet(this.reps, this.weight);
 }
