a60a0073bcf89ad9906461cb4c85bc4559e5cf60
Jack He <jackhe04@gmail.com>
2024-11-14 10:33:35 -0800

    add session

---
+++ lib/entity/session.dart
@@ -0,0 +1,15 @@
+import 'exercise.dart';
+
+abstract class Session {
+  final String _userId; // ref(User)
+  String get userId => _userId;
+  final List<Exercise> exercises;
+  List<Map<String, dynamic>> get exerciseList =>
+      List.generate(exercises.length, (i) => exercises[i].toFirestore());
+
+  Session(this._userId, List<Map<String, dynamic>> exerciseMapList)
+      : exercises = List.generate(
+            exerciseMapList.length,
+            (i) => Exercise.fromMap(exerciseMapList[i]['name'],
+                exerciseMapList[i]['type'], exerciseMapList[i]['sets']));
+}
