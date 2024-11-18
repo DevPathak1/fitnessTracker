79fbb908d97002fbe845961325fe310d4a518c0e
Jack He <jackhe04@gmail.com>
2024-11-18 13:51:44 -0800

    delete test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,30 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 
 import 'package:fitness_tracker/main.dart';
-import 'package:fitness_tracker/firebase_options.dart';
-import 'package:fitness_tracker/provider/user_provider.dart';
 
 void main() {
   testWidgets('main test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(const App());
+    await tester.pumpWidget(const App());
     print('starting test');
-    await testdb();
   });
-}
-
-Future<void> testdb() async {
-  const userId = 'wpooKB2P7GSitzppjjw8P75iv4v1';
-  print('starting testdb');
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  print('inited firebase');
-  final userProvider = UserProvider();
-  await userProvider.fetchUser(userId);
-  print('got user');
-  expect(userProvider.id, userId);
-  print(userProvider.user?.toFirestore().toString());
 }
